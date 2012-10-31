e4445bee618009775618d0c907765cbec2dc1a62
mrfrodo <golfimbulz@gmail.com>
2012-10-31 12:35:39 +0100

    mere tester

--- src/main/java/no/conduct/model/Book.java
+++ src/main/java/no/conduct/model/Book.java
@@ -1,11 +1,61 @@
 package no.conduct.model;
 
-/**
- * Created with IntelliJ IDEA.
- * User: frode
- * Date: 31.10.12
- * Time: 11:16
- * To change this template use File | Settings | File Templates.
- */
-public class Book {
+import java.io.Serializable;
+import java.sql.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Books")
+public class Book implements Serializable {
+
+    private static final long serialVersionUID = -8767337896773261247L;
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String title;
+    private Date created;
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    public Long getId() {
+        return id;
+    }
+
+    @Column(name = "firstname")
+    public String getFirstName() {
+        return firstName;
+    }
+
+    @Column(name = "lastname")
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Column(name = "created")
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
 }
--- src/test/java/no/conduct/model/BookTest.java
+++ src/test/java/no/conduct/model/BookTest.java
@@ -1,6 +1,21 @@
 
 package no.conduct.model;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 
 public class BookTest {
+
+    @Test
+    public void testVigerous() {
+        Assert.assertTrue(true);
+    }
+
+    public void testProperties() {
+        Book book = new Book();
+        Assert.assertNull(book.getFirstName());
+        Assert.assertNull(book.getLastName());
+        Assert.assertNull(book.getCreated());
+    }
 }
