9bcdd31a65e2fbba959f1b3838ba2799773bca24
mrfrodo <golfimbulz@gmail.com>
2012-10-31 11:57:46 +0100

    lagt til modell og test

---
+++ src/main/java/no/conduct/model/Book.java
@@ -0,0 +1,11 @@
+package no.conduct.model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: frode
+ * Date: 31.10.12
+ * Time: 11:16
+ * To change this template use File | Settings | File Templates.
+ */
+public class Book {
+}
---
+++ src/test/java/no/conduct/model/BookTest.java
@@ -0,0 +1,6 @@
+
+package no.conduct.model;
+
+
+public class BookTest {
+}
