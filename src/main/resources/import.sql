f52ca26b3ffaa0c1f971e95cb81d3228ad8157ec
Frode Johnsen <frode@conduct.no>
2012-11-09 13:04:34 +0100

    ilagt til nye DS i persistene.xml

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,6 @@
 
 insert into books (id, title, firstname, lastname, created) values (0, 'Jboss Seam', 'Michael Juntao', 'Yuan','1.1.2001')
 insert into books (id, title, firstname, lastname, created) values (1, 'Java 4 experts', 'frode', 'johnson','2.1.1999')
+insert into books (id, title, firstname, lastname, created) values (2, 'Java 4 experts22', 'frode22', 'johnson','2.1.1999')
 
-update hibernate_sequence set next_val = 2 where next_val = 1;
+update hibernate_sequence set next_val = (select MAX(id) from books) +1
