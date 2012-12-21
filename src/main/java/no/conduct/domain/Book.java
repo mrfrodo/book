170edd246128703979ef2497b04a989d305484b5
Frode Johnsen <frode@conduct.no>
2012-12-21 08:31:49 +0100

    fix in pom, added variable to Book.java

--- src/main/java/no/conduct/domain/Book.java
+++ src/main/java/no/conduct/domain/Book.java
@@ -14,6 +14,7 @@ public class Book implements Serializable {
 
     private static final long serialVersionUID = -8767337896773261247L;
 
+    private int iiiii;
     private Long id;
     private String firstName;
     private String lastName;
